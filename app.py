6dc3f21d47a9d9a3826ab6d0ed7b09e0800aae5c
Venkat Korampally <venkatkorampally6@gmail.com>
2026-08-13 18:19:31 +0530

    Change ARTIFACTS_DIR to current directory

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Loan Approval Predictor", page_icon="🏦", layout="centered")
 
-ARTIFACTS_DIR = "artifacts"
+ARTIFACTS_DIR = "."
 
 
 @st.cache_resource
